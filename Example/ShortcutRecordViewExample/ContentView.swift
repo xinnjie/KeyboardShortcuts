7e645bfc708262b7f2ce5adff92d38e2fd53bd72
xinnjie <xinnjie31@gmail.com>
2025-09-07 19:46:03 +0800

    feat: support multi-chords recording, like `ctrl-k ctrl-s`

---
+++ Example/ShortcutRecordViewExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShortcutRecordViewExample
+//
+//  Created by xinnjie on 2025/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/ShortcutRecordViewExample/ShortcutRecordViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutRecordViewExampleApp.swift
+//  ShortcutRecordViewExample
+//
+//  Created by xinnjie on 2025/9/8.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutRecordViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
